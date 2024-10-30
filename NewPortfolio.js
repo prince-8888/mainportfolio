f671af9ffd4c9d2ff5c7998d9e4df8ff20fbac73
prince <princeprajapati3848@gmail.com>
2024-10-30 16:32:34 +0530

    improvement in navbar and night mode

--- NewPortfolio.js
+++ NewPortfolio.js
@@ -1,10 +1,22 @@
-let dark=document.querySelector(".fa-moon");
-let bod = document.getElementsByTagName("body")[0];
-dark.addEventListener("click", () =>{
-    bod.classList.toggle("dark");
+// let dark=document.querySelector(".fa-moon");
+// let bod = document.getElementsByTagName("body")[0];
+// dark.addEventListener("click", () =>{
+
+//     bod.classList.toggle("dark");
+
+
+//     // bob.textContent = document.body.classList.contains('dark') ? '🌞' : '🌙';
     
    
+// });
+
+
+const themeToggle = document.getElementById('theme-toggle');
+themeToggle.addEventListener('click', () => {
+  document.body.classList.toggle('dark');
+  themeToggle.textContent = document.body.classList.contains('dark') ? '🌞' : '🌙';
 });
+
 
 const accordians=document.querySelectorAll('.accordian');
 for(acc of accordians){
